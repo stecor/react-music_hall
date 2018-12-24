811bf3af1baa95b75e5e1467f1c7f8282b008a51
Stefano Corra <stefano.corra@hotmail.com>
2018-12-24 16:40:25 -0500

    timer

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carrousel from './Carrousel';
+import TimeUntil from './TimeUntil'
 
 const Featured = () => {
   return(
@@ -10,7 +11,7 @@ const Featured = () => {
           Metallica
         </div>
       </div>
-
+      <TimeUntil/>
     </div>
   );
 };
