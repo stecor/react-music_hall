56944217531e578fbbb5e5311a7a75dfc93ac106
Stefano Corra <stefano.corra@hotmail.com>
2018-12-24 12:36:39 -0500

    first commit

---
+++ src/components/featured/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Features = () => (
+  <div
+    style={{
+      position:'relative'
+    }}>
+
+    <div className="artist_name">
+      <div className="wrapper">
+        
+      </div>
+    </div>
+
+  </div>
+);
+
+export default Features;
